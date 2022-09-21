d4c574b675531116e8fad485342cb9571ade6f27
Selina <pinkennnn@gmail.com>
2022-09-21 19:03:17 +0800

    'supermaket'

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import api from '@/api/index'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+import '@/assets/scss/reset.css'
+import '@/assets/scss/base.css'
+import '@/utils/rem'
+import 'swiper/dist/css/swiper.min.css'
+import './assets/icon/font_3534420_782nps767vu/iconfont.css'
+
+import ProductList from '@/components/ProductList/index.vue'
+import TabChange from '@/components/Tabs/index.vue'
+
+Vue.use(VueAwesomeSwiper)
+Vue.use(api)
+Vue.config.productionTip = false
+
+Vue.component('ProductList', ProductList)
+Vue.component('TabChange', TabChange)
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/home/components/Banner.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="banner-wrap">
+    <swiper :options="swiperOption" ref="mySwiper">
+      <!-- slides -->
+      <swiper-slide
+      v-for="(item, index) in banner"
+      :key="index"
+      >
+        <img :src="item.url" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'banner-swiper',
+  props: {
+    banner: Array
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        autoplay: {
+          delay: 3000,
+          stopOnLastSlide: false,
+          disableOnInteraction: false
+        }
+      }
+    }
+  },
+  created () {
+    // this.$axios.get('/api/home').then((res) => {
+    //   const banner = res.data.data.banner
+    //   this.banner = banner
+    // }).catch((e) => {
+    //   console.log(e)
+    // })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.banner-wrap{
+  @include wh(100%, auto);
+  img{
+    @include wh(100%, 2.4rem);
+  }
+  ::v-deep .swiper-pagination{
+    .swiper-pagination-bullet{
+      @include wh(14px, 14px);
+      display: inline-block;
+      margin-right: 8px;
+      &.swiper-pagination-bullet-active{
+        background-color: $color-primary;
+      }
+    }
+  }
+}
+</style>
